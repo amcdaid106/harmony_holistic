541f8d33d51cac678e1e13d742501bae7084bbad
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-10-11 13:54:18 +0200

    added services page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   get '/recipes', to: 'recipes#index'
   get '/healthy_tips', to: 'healthy_tips#index'
+  get '/services', to: 'pages#services'
 
 end
