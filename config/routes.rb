bb3b122ceace9b3e692eaf80f6f0eeb2ae80c0e1
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-10-11 18:18:50 +0200

    added reviews page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/healthy_tips', to: 'healthy_tips#index'
   get '/services', to: 'pages#services'
   get '/useful_links', to: 'pages#useful_links'
+  get '/reviews', to: 'pages#reviews'
   match '/contacts',     to: 'contacts#new',             via: 'get'
   resources "contacts", only: [:new, :create]
 
