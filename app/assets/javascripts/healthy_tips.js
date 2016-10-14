580d7086e5f37ca5eaacfb8c591ea437f2e4774f
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-10-14 17:38:25 +0200

    new healthy-tips layout

--- app/assets/javascripts/healthy_tips.js
+++ app/assets/javascripts/healthy_tips.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+// $("div.healthy-tip-preview").text(function(index, currentText) {
+//   var lines = currentText.split(/\r\n|\r|\n/).length;
+//   if (lines > 9) {
+//     var printLines = currentText.split('\n');
+//     return printLines.slice(0, 4);
+//     }
+// });
