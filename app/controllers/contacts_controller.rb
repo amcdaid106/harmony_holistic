68ef51d3e169fadb4b76e4706f756ff53072d988
Aoife McDaid <aoife.mcdaid@gmail.com>
2018-12-02 15:43:09 +0100

    Fix google maps url

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,7 @@
 class ContactsController < ApplicationController
   def new
     @contact = Contact.new
-    @google_maps_url = "https://www.google.com/maps/embed/v1/search?key=#{ENV['GOOGLE_MAPS_KEY']}&q=31+Hollystown+Demesne,Hollystown,Dublin+15,Co.Dublin"
-
+    @google_maps_url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2376.856155070897!2d-6.383775083999543!3d53.43528017999767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x486712d8c81cd1eb%3A0x2e1b467d82875832!2s31+Hollystown+Demesne%2C+Hollystown%2C+Dublin+15%2C+D15+P9T7%2C+Ireland!5e0!3m2!1sen!2sfr!4v1543761679279"
   end
 
   def create
